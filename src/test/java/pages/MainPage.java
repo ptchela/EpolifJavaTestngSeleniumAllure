49cdcad77a4c5511da3d285c1a871d608fb7bc32
ptchela <korneevla@yandex.ru>
2025-03-09 18:59:39 +0300

    Update CI

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -2,7 +2,12 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class MainPage extends BasePage {
 
@@ -14,6 +19,8 @@ public class MainPage extends BasePage {
     private static final By VIDEO = By.xpath("//video");
 
     public void clickUpButtonMain() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement upButton = wait.until(ExpectedConditions.elementToBeClickable(UP_BUTTON));
         driver.findElement(UP_BUTTON).click();
         String upButtonHref = driver.findElement(UP_BUTTON).getAttribute("href");
         Assert.assertEquals(upButtonHref, "https://epolif.ru/#", "Up button href is incorrect");
